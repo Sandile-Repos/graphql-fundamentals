4709bcbc0a113e7c962882da95ee62fdd9de7529
Sandile-Repos <sandile.developer@gmail.com>
2021-11-24 00:38:03 +0200

    graphql resolvers

--- server/FakeData.js
+++ server/FakeData.js
@@ -5,6 +5,22 @@ const UserList = [
     username: "john",
     age: 20,
     nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+      {
+        id: 5,
+        name: "Kelly",
+        username: "kelly1",
+        age: 15,
+        nationality: "ENGLAND",
+      },
+    ],
   },
   {
     id: 2,
@@ -18,7 +34,16 @@ const UserList = [
     name: "Sarah",
     username: "cameron",
     age: 25,
-    nationality: "UNITED STATES",
+    nationality: "INDIA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
   },
   {
     id: 4,
@@ -36,4 +61,31 @@ const UserList = [
   },
 ];
 
-module.exports = { UserList };
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: true,
+  },
+  {
+    id: 4,
+    name: "PedroTech The Movie",
+    yearOfPublication: 2035,
+    isInTheaters: false,
+  },
+];
+
+module.exports = { UserList, MovieList };
