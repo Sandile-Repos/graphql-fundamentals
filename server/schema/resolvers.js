3b63a8cbd6d884bfa5065f9b65527553c037ce15
Sandile-Repos <sandile.developer@gmail.com>
2021-11-24 02:56:57 +0200

    graphql mutations

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -22,6 +22,7 @@ const resolvers = {
       return movie;
     },
   },
+
   User: {
     favoriteMovies: () => {
       return _.filter(
@@ -29,6 +30,35 @@ const resolvers = {
         (movie) =>
           movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
       );
+    },
+  },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+
+    updateUsername: (parent, args) => {
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+
+      return userUpdated;
+    },
+
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id === Number(id));
+      return null;
     },
   },
 };
